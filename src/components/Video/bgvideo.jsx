55f483c7091be1e17ecdc27734853aca1558132c
Girish <girishcs97@gmail.com>
2024-05-15 19:14:10 -0400

    play btn

--- src/components/Video/bgvideo.jsx
+++ src/components/Video/bgvideo.jsx
@@ -3,6 +3,21 @@ import './bgvideo.css';
 import { BsPlayCircle } from "react-icons/bs";
 
 export default function BackgroundVideo() {
+    const videoUrl = "https://www.youtube.com/embed/bqHZ-fiztq8";
+
+    const openVideoPopup = () => {
+        const width = window.innerWidth * 0.8;
+        const height = window.innerHeight * 0.8;
+        const left = (window.innerWidth - width) / 2;
+        const top = (window.innerHeight - height) / 2;
+        window.open(
+            videoUrl,
+            "_blank",
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+            scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+            height=${height}, top=${top}, left=${left}`
+        );
+    };
     return (
         <div className="container-fluid">
             <header id="header-bg">
@@ -12,7 +27,7 @@ export default function BackgroundVideo() {
                             <div className="row">
                                 <div className="col-md-12 col-md-offset-2 intro-text">
                                     <div className="d-flex justify-content-center">
-                                        <BsPlayCircle className="play-btn" />
+                                        <BsPlayCircle className="play-btn" onClick={openVideoPopup}/>
                                     </div>
                                     <div className="d-flex justify-content-center">
                                         <h3>
