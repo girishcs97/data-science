b707b4b8ef17432d6f234be59fe34a914fc360ac
Gitesh Sagvekar <giteshvs307@gmail.com>
2024-05-21 15:49:11 -0400

    added animation effects

--- src/components/Career/career.jsx
+++ src/components/Career/career.jsx
@@ -14,20 +14,25 @@ import wayfair from '../../images/Wayfair.png';
 import tripadvisor from '../../images/tripadvisor.png';
 import wsu from '../../images/wsu.jpeg';
 import dell from '../../images/dell.jpeg';
+import Fade from "react-reveal/Fade";
 
 const Career = () => {
     return (
         <div className="container margin-top-40" id='research'>
             <div className="row">
+            <Fade bottom duration={800}>
                 <div className='col-md-5'>
                     <p className="display-1">Careers</p>
                 </div>
                 <div className='col-md-7'>
                     <p className="display-p">we offer exciting career and internship opportunities for individuals who are passionate about making a difference and driving positive change. Whether you're seeking a full-time position or looking to gain hands-on experience through an internship, we invite you to explore the possibilities and join our dynamic team.</p>
                 </div>
+                </Fade>
             </div>
             <div className="margin-top-40">
+            <Fade right duration={800}>
                 <img src={career} className="image-cov" />
+            </Fade>
             </div>
             <div className="container opp-content">
                 With a degree in data science from Clark, you'll possess the analytical skills and technical expertise needed to excel in a variety of industries. Whether you're interested in uncovering insights from complex datasets as a data scientist, designing machine learning algorithms as a machine learning engineer, or providing strategic guidance as a business intelligence analyst, your opportunities are boundless.
@@ -37,12 +42,14 @@ const Career = () => {
                     <div className='col-md-7'>
                         <div className='row'>
                             <div className='col-md-5 mr-1'>
+                            <Fade bottom duration={800}>
                                 <img src={career1} className='' style={{'width':'634px'}}/>
+                            </Fade>
                             </div>
                         </div>
                     </div>
                     <div className='col-md-5 mart-top'>
-
+                    <Fade bottom duration={800}>
                         <h2 style={{ lineHeight: '1.182em' }}>Career and Internship Opportunities</h2>
                         <div class="line mg-bottom-32px"></div>
                         <p style={{ textAlign: 'justify', marginTop: '20px', lineHeight: '1.667em', color: '#4b5b70', fontSize: '16px', fontWeight: '400' }}>At Clark, we offer an array of career and internship opportunities designed to cultivate your talents and drive your aspirations forward. Whether you're a seasoned professional seeking new challenges or a student eager to gain hands-on experience, we invite you to explore the possibilities and join our vibrant community of changemakers.</p>
@@ -50,6 +57,7 @@ const Career = () => {
                         <div className='d-flex flex-row'>
                             <button className="btn btn-outline-danger custom-btn">VISIT THE CENTER</button>
                         </div>
+                    </Fade>
                     </div>
                 </div>
 
@@ -58,6 +66,7 @@ const Career = () => {
             <div className='container mart-top mb-4'>
                 <div className='row'>
                     <div className='col-md-6 mart-top'>
+                    <Fade bottom duration={800}>
                         <h2 style={{ lineHeight: '1.182em' }}>Clark Funding for Internships</h2>
                         <div class="line mg-bottom-32px"></div>
                         <p style={{ textAlign: 'justify', marginTop: '20px', lineHeight: '1.667em', color: '#4b5b70', fontSize: '16px', fontWeight: '400' }}>At Clark, we understand the importance of hands-on experience in shaping the future of our students. That's why we offer funding opportunities to support internships and experiential learning opportunities for our students. Our internship funding programs are designed to remove financial barriers and empower students to pursue valuable professional experiences that complement their academic studies.</p>
@@ -65,18 +74,22 @@ const Career = () => {
                         <div className='d-flex flex-row'>
                             <button className="btn btn-outline-danger custom-btn">PURSUE FUNDING</button>
                         </div>
+                    </Fade>
                     </div>
                     <div className="col-md-1"></div>
                     <div className='col-md-5'>
                         <div className='row'>
                             <div className='col-md-6'>
+                            <Fade bottom duration={800}>
                                 <img src={career2} className='mt-2' />
+                            </Fade>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="customer-section margin-top-40">
+            <Fade bottom duration={800}>
                 <h2>OUR KEY RECRUITERS</h2>
                 <div className="customer-logos">
                     <Link to='https://about.google/intl/en_us/'>
@@ -100,6 +113,7 @@ const Career = () => {
                     <Link to='https://www.dell.com/en-us'>
                         <img src={dell} alt="dell" /></Link>
                 </div>
+            </Fade>
             </div>
         </div>
 
